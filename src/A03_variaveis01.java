e151d02c559c9191eb6a13338f811fc58c319a58
srcmilena <87768754+srcmilena@users.noreply.github.com>
2022-05-12 22:45:37 -0300

    first commit

---
+++ src/A03_variaveis01.java
@@ -0,0 +1,13 @@
+public class A03_variaveis01 {
+
+    public static void main(String[] args) {
+
+        //String nome = "Milena";
+
+        String nome; // Apenas declarando a variável
+        nome = "Let´s code"; // Atribuindo valor
+        nome = "Milena"; // Atribuindo um novo valor e substituindo o anterior
+        nome = "Brasil"; // Novamente atribuindo um novovalor e substituindo o anterior
+        System.out.println("Olá, " + nome);
+    }
+}
---
+++ src/A03_variaveis02.java
@@ -0,0 +1,25 @@
+public class A03_variaveis02 {
+
+    public static void main(String[] args) {
+
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        /* int divisao = a / b; resultado = 1, pois int é para números inteiros
+           float divisao = a / b; resultado = 1.0, pois float indica ao Java que, um número inteiro dividido por outro número inteiro, obrigatoriamente, retorna um inteiro (particularidade do Java)
+
+           Soluções:
+           1) Mais simples: alterar todas as entradas de dados para float. Porém, dificulta num código grande
+           2) cast: usar parentêses na expressão. Transformar e representar como se fosse aquele tipo. O resultado seria int, mas representa como se fosse float */
+
+        float divisao = (float) a / b;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+    }
+}
